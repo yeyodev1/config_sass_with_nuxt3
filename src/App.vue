3b88d906e9ac157f367d8faa8fba8a0d650ae199
Diego Reyes <diegorele13@gmail.com>
2024-03-15 19:25:23 -0500

    add: config

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,73 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container">
+    <div class="welcome">
+      <h1>Bienvenidos a mi Blogggg</h1>
+      <img 
+        src="https://i.pinimg.com/236x/b6/ef/77/b6ef77457af02e5a597703e4903a1dad.jpg"
+        alt="Imagen descriptiva">
+      <p class="">
+        Esta es una super descripción genérica para el blog de como usar sass. Aquí podrás encontrar contenido sobre programación, tecnología, y mucho más.
+      </p>
+      <div class="social-media">
+        <a 
+          href="https://facebook.com"
+          target="_blank">
+          <img 
+            src="https://i.pinimg.com/236x/56/30/d2/5630d2355f9da80b900fd4523389d169.jpg" 
+            alt="Facebook">
+        </a>
+        <a 
+          href="https://twitter.com"
+          target="_blank">
+          <img 
+            src="https://i.pinimg.com/236x/07/df/57/07df57f369b03a4cc85a94bfb7002db9.jpg" 
+            alt="Twitter">
+        </a>
+        <a 
+          href="https://instagram.com"
+          target="_blank">
+          <img 
+            src="https://i.pinimg.com/236x/73/ec/fe/73ecfe9b8b2801adf54eb66e58efc4dd.jpg"
+            alt="Instagram">
+        </a>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style>
+.welcome {
+  text-align: center;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.welcome img {
+  margin-top: 20px;
+  width: 150px;
+  height: auto;
+  border-radius: 10px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.social-media {
+  margin-top: 20px;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.social-media a {
+  margin: 0 10px;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.social-media img {
+  width: 40px;
+  height: 40px;
+  transition: transform 0.3s ease;
+}
+
+.social-media img:hover {
+  transform: scale(1.1);
 }
 </style>
